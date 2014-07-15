a48a843bd9d2cecff76a7dba5b5af41f84c261d9
viggo <dev@cviggo.com>
2014-07-15 22:56:06 +0200

    * initial commit * added all event hooks

---
+++ src/main/java/org/bungeenox/BungeeNox.java
@@ -0,0 +1,28 @@
+package org.bungeenox;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.logging.Logger;
+
+public class BungeeNox extends Plugin {
+
+    private Logger logger;
+
+    @Override
+    public void onEnable() {
+        getProxy().getPluginManager().registerListener(this, new BungeeNoxListener(this));
+        logger = getLogger();
+    }
+
+    public void logInfo(String message){
+        logger.info(message);
+    }
+
+    public void logWarn(String message){
+        logger.warning(message);
+    }
+
+    public void logSevere(String message){
+        logger.severe(message);
+    }
+}
---
+++ src/main/java/org/bungeenox/BungeeNoxListener.java
@@ -0,0 +1,110 @@
+package org.bungeenox;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.event.*;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+import java.security.acl.Permission;
+
+public class BungeeNoxListener implements Listener {
+    private final BungeeNox bungeeNox;
+
+    public BungeeNoxListener(BungeeNox bungeeNox) {
+        this.bungeeNox = bungeeNox;
+    }
+
+    @EventHandler
+    public void onChatEvent(final ChatEvent event) {
+        bungeeNox.logInfo("onChatEvent");
+    }
+
+    @EventHandler
+    public void onLoginEvent(final LoginEvent event) {
+        bungeeNox.logInfo("onLoginEvent");
+    }
+
+    @EventHandler
+    public void onPermissionCheckEvent(final PermissionCheckEvent event) {
+        bungeeNox.logInfo("onPermissionCheckEvent");
+    }
+
+    @EventHandler
+    public void onPlayerDisconnect(final PlayerDisconnectEvent event) {
+        bungeeNox.logInfo("onPlayerDisconnect");
+    }
+
+    @EventHandler
+    public void onPlayerHandshake(final PlayerHandshakeEvent event) {
+        bungeeNox.logInfo("onPlayerHandshake");
+    }
+    @EventHandler
+    public void onPluginMessage(final PluginMessageEvent event) {
+        bungeeNox.logInfo("onPluginMessage");
+    }
+    @EventHandler
+    public void onPostLogin(final PostLoginEvent event) {
+        bungeeNox.logInfo("onPostLogin");
+    }
+    @EventHandler
+    public void onPreLogin(final PreLoginEvent event) {
+        bungeeNox.logInfo("onPreLogin");
+    }
+    @EventHandler
+    public void onProxyPing(final ProxyPingEvent event) {
+        bungeeNox.logInfo("onProxyPing");
+    }
+    @EventHandler
+    public void onProxyReload(final ProxyReloadEvent event) {
+        bungeeNox.logInfo("onProxyReload");
+    }
+
+    /*
+    http://ci.md-5.net/job/BungeeCord/ws/api/target/apidocs/net/md_5/bungee/api/event/ServerConnectedEvent.html
+
+    Not to be confused with ServerConnectEvent, this event is called once a connection to a server is fully operational,
+    and is about to hand over control of the session to the player. It is useful if you wish to send information to
+    the server before the player logs in.
+    */
+    @EventHandler
+    public void onServerConnected(final ServerConnectedEvent event) {
+        bungeeNox.logInfo("onServerConnected");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            bungeeNox.logSevere(e.toString());
+        }
+        //event.getPlayer().sendMessage(new ComponentBuilder("Welcome to " + event.getServer().getInfo().getName() + "!").color(ChatColor.GREEN).create());
+    }
+
+    @EventHandler
+    public void onServerConnect(final ServerConnectEvent event) {
+        bungeeNox.logInfo("onServerConnect");
+    }
+
+    @EventHandler
+    public void onServerDisconnect(final ServerDisconnectEvent event) {
+        bungeeNox.logInfo("onServerDisconnect");
+    }
+
+    @EventHandler
+    public void onServerKick(final ServerKickEvent event) {
+        bungeeNox.logInfo("onServerKick");
+    }
+
+    @EventHandler
+    public void onServerSwitch(final ServerSwitchEvent event) {
+        bungeeNox.logInfo("onServerSwitch");
+    }
+
+    @EventHandler
+    public void onTabComplete(final TabCompleteEvent event) {
+        bungeeNox.logInfo("onTabComplete");
+    }
+
+    @EventHandler
+    public void onTargeted(final TargetedEvent event) {
+        bungeeNox.logInfo("onTargeted");
+    }
+}
