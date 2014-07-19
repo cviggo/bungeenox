d3fe15cc0da1dcc90df7c30bccc2b632178266a2
viggo <dev@cviggo.com>
2014-07-19 23:58:19 +0200

    * async warp delay

--- src/main/java/org/bungeenox/BungeeNoxListener.java
+++ src/main/java/org/bungeenox/BungeeNoxListener.java
@@ -9,24 +9,27 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
 public class BungeeNoxListener implements Listener {
-    private final BungeeNox bungeeNox;
+    private final BungeeNox plugin;
 
-    public BungeeNoxListener(BungeeNox bungeeNox) {
-        this.bungeeNox = bungeeNox;
+    public BungeeNoxListener(BungeeNox plugin) {
+        this.plugin = plugin;
     }
 
     @EventHandler
     public void onChatEvent(final ChatEvent event) {
 
         final String message = event.getMessage();
-        bungeeNox.logInfo(event.getSender().toString() + " : " + message);
+        plugin.logInfo(event.getSender().toString() + " : " + message);
 
-        if ("/saveallplayers".equals(message)){
-            final Collection<ProxiedPlayer> proxiedPlayers = bungeeNox.getProxy().getPlayers();
+        if ("/saveallplayers".equals(message)) {
+            final Collection<ProxiedPlayer> proxiedPlayers = plugin.getProxy().getPlayers();
 
-            bungeeNox.logInfo("Saving all players to all servers");
+            plugin.logInfo("Saving all players to all servers");
 
             for (ProxiedPlayer proxiedPlayer : proxiedPlayers) {
                 final Server server = proxiedPlayer.getServer();
@@ -39,57 +42,106 @@ public class BungeeNoxListener implements Listener {
                 server.sendData("BungeeCord", out.toByteArray());
             }
 
-            bungeeNox.logInfo("Done saving all players to all servers");
+            plugin.logInfo("Done saving all players to all servers");
         }
     }
 
     @EventHandler
     public void onLoginEvent(final LoginEvent event) {
-        bungeeNox.logInfo("onLoginEvent");
+        plugin.logInfo("onLoginEvent");
     }
 
     @EventHandler
     public void onPermissionCheckEvent(final PermissionCheckEvent event) {
-        //bungeeNox.logInfo("onPermissionCheckEvent: " + event.getPermission());
+        //plugin.logInfo("onPermissionCheckEvent: " + event.getPermission());
 
     }
 
     @EventHandler
     public void onPlayerDisconnect(final PlayerDisconnectEvent event) {
-        bungeeNox.logInfo("onPlayerDisconnect");
+        plugin.logInfo("onPlayerDisconnect");
     }
 
     @EventHandler
     public void onPlayerHandshake(final PlayerHandshakeEvent event) {
-        bungeeNox.logInfo("onPlayerHandshake");
+        plugin.logInfo("onPlayerHandshake");
     }
 
     @EventHandler
     public void onPluginMessage(final PluginMessageEvent event) {
 
-        //bungeeNox.logInfo("onPluginMessage: " + event.toString());
+        //plugin.logInfo("onPluginMessage: " + event.getTag());
+
+        //final String tag = event.getTag();
+//        if ("CC".equals(tag)
+//                || "MEK".equals(tag)
+//                || "AE".equals(tag)
+//                || "BC".equals(tag)
+//                || "OpenMods".equals(tag)
+//                || "EnderIO".equals(tag)
+//                || "gravisuite".equals(tag)
+//                || "OpenMods|s".equals(tag)
+//                || "BC".equals(tag)
+//
+//        )
+//        {
+//            return;
+//        }
+
+//        if (!tag.toLowerCase().contains("cloud")) {
+//            return;
+//        }
+//
+//        final byte[] message = event.getData();
+//
+//        if (message == null) {
+//            return;
+//        }
+//
+//        ByteArrayDataInput in = ByteStreams.newDataInput(message);
+//
+//        if (in == null) {
+//            return;
+//        }
+//
+//        String part = null;
+//
+//        while (true) {
+//            try {
+//                part += in.readUTF() + ", ";
+//                break;
+//
+//            } catch (Throwable t) {
+//                break;
+//            }
+//        }
+//
+//        if (part != null) {
+//            plugin.logInfo("-----PluginMsg:" + tag + ": " + part);
+//        }
+
     }
 
     @EventHandler
     public void onPostLogin(final PostLoginEvent event) {
-        bungeeNox.logInfo("onPostLogin");
+        plugin.logInfo("onPostLogin");
 
         //event.getPlayer().disconnect("World server reboot in progress. Hang on :) - it will only take a few minutes");
     }
 
 //    @EventHandler
 //    public void onPreLogin(final PreLoginEvent event) {
-//        bungeeNox.logInfo("onPreLogin");
+//        plugin.logInfo("onPreLogin");
 //    }
 
     @EventHandler
     public void onProxyPing(final ProxyPingEvent event) {
-        bungeeNox.logInfo("onProxyPing");
+        plugin.logInfo("onProxyPing");
     }
 
 //    @EventHandler
 //    public void onProxyReload(final ProxyReloadEvent event) {
-//        bungeeNox.logInfo("onProxyReload");
+//        plugin.logInfo("onProxyReload");
 //    }
 
     /*
@@ -101,19 +153,21 @@ public class BungeeNoxListener implements Listener {
     */
     @EventHandler
     public void onServerConnected(final ServerConnectedEvent event) {
-        bungeeNox.logInfo("onServerConnected");
+        plugin.logInfo("onServerConnected");
 //        try {
 //            Thread.sleep(2000);
-//            bungeeNox.logInfo("done waiting");
+//            plugin.logInfo("done waiting");
 //        } catch (InterruptedException e) {
-//            bungeeNox.logSevere(e.toString());
+//            plugin.logSevere(e.toString());
 //        }
         //event.getPlayer().sendMessage(new ComponentBuilder("Welcome to " + event.getServer().getInfo().getName() + "!").color(ChatColor.GREEN).create());
     }
 
+    ConcurrentMap<String, Object> delayedWarps = new ConcurrentHashMap<String, Object>();
+
     @EventHandler
     public void onServerConnect(final ServerConnectEvent event) {
-        bungeeNox.logInfo("onServerConnect");
+        plugin.logInfo("onServerConnect");
 
         final ProxiedPlayer player = event.getPlayer();
 
@@ -127,11 +181,27 @@ public class BungeeNoxListener implements Listener {
             return;
         }
 
-        //event.setCancelled(true);
+        if (server.getInfo().getName().toLowerCase().equals(event.getTarget().getName().toLowerCase())){
+            return;
+        }
 
         try {
 
-            bungeeNox.logInfo("player came from server: " + server.getInfo().getName());
+
+            String key = server.getInfo().getName().toLowerCase() + player.getName().toLowerCase() + event.getTarget().getName().toLowerCase();
+
+            plugin.logInfo(key);
+
+            if(delayedWarps.containsKey(key)){
+                //plugin.logInfo("removed key: " + key);
+                delayedWarps.remove(key);
+                return;
+            }
+
+            //plugin.logInfo("adding key: " + key);
+            delayedWarps.put(key, key);
+
+            //plugin.logInfo("player came from server: " + server.getInfo().getName());
 
             ByteArrayDataOutput out = ByteStreams.newDataOutput();
             out.writeUTF("BungeeNox");
@@ -140,37 +210,62 @@ public class BungeeNoxListener implements Listener {
 
             server.sendData("BungeeCord", out.toByteArray());
 
-            Thread.sleep(5000);
+            event.setCancelled(true);
 
-            bungeeNox.logInfo("onServerConnect done waiting");
+            ByteArrayDataOutput out2 = ByteStreams.newDataOutput();
+            out2.writeUTF("BungeeNox");
+            out2.writeUTF("WarpingPlayer");
+            out2.writeUTF(player.getName());
+            out2.writeUTF(event.getTarget().getName());
 
-        } catch (InterruptedException e) {
-            bungeeNox.logSevere(e.toString());
+            server.sendData("BungeeCord", out.toByteArray());
+
+            player.sendMessage("Warping to " + event.getTarget().getName() + " in 5 seconds.");
+
+            plugin.getProxy().getScheduler().schedule(plugin, new Runnable() {
+                        @Override
+                        public void run() {
+                            plugin.logInfo("Connecting player " + player.getName() + " to server " + event.getTarget().getName() );
+                            player.connect(event.getTarget());
+                        }
+                    },
+                    5,
+                    TimeUnit.SECONDS
+            );
+
+            //Thread.sleep(5000);
+
+            //bungeeNox.logInfo("onServerConnect done waiting");
+
+        } catch (Throwable t) {
+            //String str = ExceptionUtils.getStackTrace(t);
+            plugin.logSevere(t.toString());
+            t.printStackTrace();
         }
     }
 
 //    @EventHandler
 //    public void onServerDisconnect(final ServerDisconnectEvent event) {
-//        bungeeNox.logInfo("onServerDisconnect");
+//        plugin.logInfo("onServerDisconnect");
 //    }
 
     @EventHandler
     public void onServerKick(final ServerKickEvent event) {
-        bungeeNox.logInfo("onServerKick");
+        plugin.logInfo("onServerKick");
     }
 
     @EventHandler
     public void onServerSwitch(final ServerSwitchEvent event) {
-        bungeeNox.logInfo("onServerSwitch");
+        plugin.logInfo("onServerSwitch");
     }
 
 //    @EventHandler
 //    public void onTabComplete(final TabCompleteEvent event) {
-//        bungeeNox.logInfo("onTabComplete");
+//        plugin.logInfo("onTabComplete");
 //    }
 
     @EventHandler
     public void onTargeted(final TargetedEvent event) {
-        bungeeNox.logInfo("onTargeted");
+        plugin.logInfo("onTargeted");
     }
 }
